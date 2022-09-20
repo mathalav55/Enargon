4a95543acd9d079a70f42400715b4036dbefacbe
Vamsi Krishna <46787868+mathalav55@users.noreply.github.com>
2022-09-20 18:25:13 +0530

    added month and week categ

--- lpg/lpgController.js
+++ lpg/lpgController.js
@@ -3,31 +3,29 @@ var router = express.Router();
 
 var lpg = require('./lpgSchema');
 
-
-var params = {
-    year : 2021
-};
-
-var firstDay = getFirstDay(params);
-var lastDay = getLastDay(params);
-
 router.get('/',async(req,res,next)=>{
+    var firstDay = getFirstDay(req.body);
+    var lastDay = getLastDay(req.body);
+    
     const data = await lpg.find({ Date : { $gte : firstDay, $lte : lastDay} },);
     res.status(200).json({
-        message : "all good ..."
+        message : "all good ...",
+        len : data.length,
     });  
 });
 
 function getFirstDay(data){
-    var year = 2021 || data.year;
-    var month = 0 || data.month;
+    var year = data.year ? data.year : 2021;
+    var month = data.month ? data.month : 0;
     var res = new Date(year,month,1,5,30);
+    console.log(res.toLocaleDateString());
     return res.getTime();
 }
 function getLastDay(data){
-    var year = 2021 || data.year;
-    var month = 12 || data.month;
+    var year = data.year ? data.year : 2021;
+    var month = data.month ? data.month : 12;
     var res = new Date(year,month+1,0,5,30);
+    console.log(res.toLocaleDateString());
     return res.getTime();
 }
 module.exports = router;
--- lpg/lpgSchema.js
+++ lpg/lpgSchema.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 var lpgSchema = mongoose.Schema({
         Date : Number,
-        week : Number,
 });
 
 mongoose.model('dailydata',lpgSchema);
