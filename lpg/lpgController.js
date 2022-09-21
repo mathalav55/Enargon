f37c70bff6cce1e5a73bccdd096a4d2982162dfa
mathalav55 <matalav55@gmail.com>
2022-09-21 17:30:44 +0530

    modified production and depatch fields

--- lpg/lpgController.js
+++ lpg/lpgController.js
@@ -5,11 +5,13 @@ var router = express.Router();
 var lpg = require("./lpgSchema");
 
 router.get("/", async (req, res, next) => {
+  //get year sum
+
   var firstDay = getFirstDay(req.body);
   var lastDay = getLastDay(req.body);
   var data = await lpg.find({
     Date: { $gte: firstDay.getTime(), $lte: lastDay.getTime() },
-    working : req.body.working
+    working: req.body.working,
   });
   // var data = await lpg.find();
   if (req.body.week) {
@@ -22,12 +24,15 @@ router.get("/", async (req, res, next) => {
 });
 
 // util functions
-function aggregateData(data,period) {
+function aggregateData(data) {
+  //prodcution sum
+  //dispatch sum
   //gets an array
-  const length= data.length;
+  const length = data.length;
   var expandingMean = 0;
   var sum = 0;
-  data.forEach(item =>{
+  //sum for all categories
+  data.forEach((item) => {
     sum += parseInt(data.type);
   });
   //sends aggregate
@@ -38,22 +43,39 @@ function formatData(data) {
     "14.2kg",
     "19kg",
     "5kg(RED)",
-    "5kg(FTL/NDM/BLUE)",
     "35kg",
     "47.5",
     "425kg",
     "5kg(FTL/FTR/BLUE)",
     "2kg(FTL/FTR/BLUE)",
   ];
-  var dispatchfields =[];
-  var productionfields= [];
+  var dispatchfields = [
+    "Days_Production_14.2kg",
+    "Days_Production_19kg",
+    "Days_Production_5kg(RED)",
+    "Days_Production_35kg",
+    "Days_Production_47.5",
+    "Days_Production_425kg",
+    "Days_Production_5kg(FTL/FTR/BLUE)",
+    "Days_Production_2kg(FTL/FTR/BLUE)",
+  ];
+  var productionfields = [
+    "Days_Despatches_14.2kg",
+    "Days_Despatches_19kg",
+    "Days_Despatches_5kg(RED)",
+    "Days_Despatches_35kg",
+    "Days_Despatches_47.5",
+    "Days_Despatches_425kg",
+    "Days_Despatches_5kg(FTL/FTR/BLUE)",
+    "Days_Despatches_2kg(FTL/FTR/BLUE)",
+  ];
   var categoryMap = [];
   //mapping label and field
   for (var i = 0; i < categories.length; i++) {
     var mapObj = {
-        label : labels[i],
-        dispatchFieldName : dispatchfields[i],
-        productionFieldName : productionfields[i],
+      label: labels[i],
+      dispatchFieldName: dispatchfields[i],
+      productionFieldName: productionfields[i],
     };
     categoryMap.push(mapObj);
   }
@@ -61,7 +83,7 @@ function formatData(data) {
     var newObj = {
       type: category.label,
       prod: data[category.productionFieldName],
-      dis : data[category.dispatchFieldName]
+      dis: data[category.dispatchFieldName],
     };
     newData.push(newObj);
   });
