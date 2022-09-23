a0f4be7bf0d9a67cdd132a377ed332706addca8f
mathalav55 <matalav55@gmail.com>
2022-09-23 12:01:14 +0530

    added week functionality to frontend

--- public/index.js
+++ public/index.js
@@ -3,48 +3,6 @@ let selectMonthsEle = document.getElementById("SelectMonth");
 let DayList = document.getElementById("SelectDay");
 var WeekList = document.getElementById("SelectWeek");
 let url = "/lpg";
-const barCfg = {
-  type: "bar",
-  data: {
-    datasets: [
-      {
-        label: "Global",
-        data: [],
-        parsing: {
-          yAxisKey: "global",
-          xAxisKey: "type",
-        },
-        backgroundColor: "#f00",
-      },
-      {
-        label: "Current",
-        data: [],
-        parsing: {
-          yAxisKey: "current",
-          xAxisKey: "type",
-        },
-        backgroundColor: "#f0f",
-      },
-    ],
-  },
-};
-var ctx2 = document.getElementById("chart2");
-var ctx = document.getElementById("chart");
-var prodChartElement = new Chart(ctx, barCfg);
-var disChartElement = new Chart(ctx2, barCfg);
-
-console.log("going til here");
-// async function fetching(){
-//   try{
-//     fetch(url)
-//     .then((res) =>  res.json())
-//     .then((data) =>  console.log(data.res.production))
-//     .catch((err) => console.log(err));
-//   }catch(err){
-//     console.log(err)
-//   }
-// }
-// fetching()
 
 let Years = ["2021", "2022"];
 let months = [
@@ -76,15 +34,18 @@ let weekval;
 let dayval;
 // getting the year value for fetching number of days
 let SelectionofDaysYearValue;
+let yearValueForGettingWeek;
 function YearValue() {
   let selectedValue = selectYearEle.options[selectYearEle.selectedIndex].text;
   year = selectedValue;
   SelectionofDaysYearValue = selectedValue;
   yearval = selectedValue;
   console.log(selectedValue);
+  yearValueForGettingWeek = selectedValue;
 }
 
 let min = 1;
+let MonthValueForGettingWeek;
 function MonthValue() {
   // getting the month value ex: jan : 1,feb: 2....
 
@@ -97,6 +58,7 @@ function MonthValue() {
   // console.log(SelectionofDaysYearValue)
   let yearNumber = Number(SelectionofDaysYearValue);
   let monthNumber = Number(selectedValue) + 1;
+  MonthValueForGettingWeek = monthNumber;
   console.log(yearNumber, monthNumber);
   let x = new Date(yearNumber, monthNumber, 0).getDate();
   // console.log(x)
@@ -110,10 +72,69 @@ function MonthValue() {
     final += dayOption;
   }
   DayList.innerHTML = final;
+
+  WeekValue();
 }
 
 function WeekValue() {
-  let selectedValue = WeekList.options[WeekList.selectedIndex].text;
+  function endFirstWeek(firstDate, firstDay) {
+    if (!firstDay) {
+      return 7 - firstDate.getDay();
+    }
+    if (firstDate.getDay() < firstDay) {
+      return firstDay - firstDate.getDay();
+    } else {
+      return 7 - firstDate.getDay() + firstDay;
+    }
+  }
+
+  function getWeeksStartAndEndInMonth(month, year) {
+    let weeks = [],
+      firstDate = new Date(year, month, 1),
+      lastDate = new Date(year, month + 1, 0),
+      numDays = lastDate.getDate();
+
+    let start = 1;
+    let end = endFirstWeek(firstDate, 1);
+    while (start <= numDays) {
+      weeks.push({ start: start, end: end });
+      start = end + 1;
+      end = end + 7;
+      end = start === 1 && end === 8 ? 1 : end;
+      if (end > numDays) {
+        end = numDays;
+      }
+    }
+    return weeks;
+  }
+  let weeksList = getWeeksStartAndEndInMonth(
+    MonthValueForGettingWeek - 1,
+    yearValueForGettingWeek
+  );
+  console.log(weeksList);
+  let startDay, endDay;
+  const option = document.createElement("option");
+  var final = "";
+  for (let i = 0; i < weeksList.length; i++) {
+    startDay = new Date(
+      yearValueForGettingWeek,
+      MonthValueForGettingWeek - 1,
+      weeksList[i].start
+    );
+    endDay = new Date(
+      yearValueForGettingWeek,
+      MonthValueForGettingWeek - 1,
+      weeksList[i].end
+    );
+    option.textContent = `${i+1}st Week  From [${startDay.toLocaleDateString()}] To [${endDay.toLocaleDateString()}]`;
+    let weekOption = `<option value="${i}">${option.textContent}</option>`;
+    // option.setAttribute("id");
+    final += weekOption;
+    // WeekList.appendChild(option);
+  }
+  WeekList.innerHTML = final;
+
+  let selectedValue = WeekList.options[WeekList.selectedIndex].value;
   week = selectedValue;
   weekval = selectedValue;
   console.log(selectedValue);
@@ -150,6 +171,8 @@ function dayBtn() {
   weekBtn();
   let day = document.getElementById("DaysSection");
   day.style.display = "block";
+  let week = document.getElementById("WeeksSection");
+  week.style.display = "none";
 }
 
 let fetchedProductionData;
@@ -162,26 +185,17 @@ async function GetDetails() {
   };
   console.log(obj);
   document.getElementById("chartToggleID").style.display = "block";
-let options  = {
-  method : "POST",  
-  headers : {
-    "Content-Type" : "application/json"
-  },
-  body : JSON.stringify(obj)
-}
+  let options = {
+    method: "GET",
+    body: JSON.stringify(obj),
+  };
 
-  fetch(url,options)
+  fetch(url, options)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.res)
-      if(data.res){
-        productionChart(data.res.production);
-        dispatchChart(data.res.dispatch);
-      }else{
-        productionChart([]);
-        dispatchChart([]);
-        alert("no data found");
-      }
+      console.log(data.res.production);
+      productionChart(data.res.production);
+      dispatchChart(data.res.dispatch);
     })
     .catch((err) => console.log(err));
 }
@@ -189,21 +203,72 @@ let options  = {
 // charts data
 function productionChart(data) {
   var ctx = document.getElementById("chart");
-  // const labels = [];
-  // data.forEach((item) => {
-  //   labels.push(item.type);
-  // });
-  barCfg.data.datasets[0].data = data;
-  barCfg.data.datasets[1].data = data;
-  prodChartElement.update();
+  const labels = [];
+  data.forEach((item) => {
+    labels.push(item.type);
+  });
+  const cfg = {
+    type: "bar",
+    data: {
+      labels,
+      datasets: [
+        {
+          label: "Global",
+          data: data,
+          parsing: {
+            yAxisKey: "global",
+            xAxisKey: "type",
+          },
+          backgroundColor: "#f00",
+        },
+        {
+          label: "Current",
+          data: data,
+          parsing: {
+            yAxisKey: "current",
+            xAxisKey: "type",
+          },
+          backgroundColor: "#f0f",
+        },
+      ],
+    },
+  };
+  var chart = new Chart(ctx, cfg);
 }
 
 function dispatchChart(data) {
+  var ctx2 = document.getElementById("chart2");
+
   const labels2 = [];
   data.forEach((item) => {
     labels2.push(item.type);
   });
-  barCfg.data.datasets[0].data = data;
-  barCfg.data.datasets[1].data = data;
-  disChartElement.update();
+  const cfg2 = {
+    type: "bar",
+    data: {
+      labels2,
+      datasets: [
+        {
+          label: "Global",
+          data: data,
+          parsing: {
+            yAxisKey: "global",
+            xAxisKey: "type",
+          },
+          backgroundColor: "#f00",
+        },
+        {
+          label: "Current",
+          data: data,
+          parsing: {
+            yAxisKey: "current",
+            xAxisKey: "type",
+          },
+          backgroundColor: "#f0f",
+        },
+      ],
+    },
+  };
+  var chart = new Chart(ctx2, cfg2);
 }
+
